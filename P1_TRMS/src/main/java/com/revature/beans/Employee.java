bb589fb43db261112dc80a3d29817b2353d17190
unknown <dustinbayne51@gmail.com>
2018-06-08 18:55:18 -0400

    employee bean

--- P1_TRMS/src/main/java/com/revature/beans/Employee.java
+++ P1_TRMS/src/main/java/com/revature/beans/Employee.java
@@ -1,5 +1,76 @@
 package com.revature.beans;
 
 public class Employee {
+	private int empID;
+	private String name;
+	private int positionID;
+	private int managerID;
+	private String userName;
+	private String password;
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(int empID, String name, int positionID, int managerID, String userName, String password) {
+		super();
+		this.empID = empID;
+		this.name = name;
+		this.positionID = positionID;
+		this.managerID = managerID;
+		this.userName = userName;
+		this.password = password;
+	}
 
+	public int getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPositionID() {
+		return positionID;
+	}
+
+	public void setPositionID(int positionID) {
+		this.positionID = positionID;
+	}
+
+	public int getManagerID() {
+		return managerID;
+	}
+
+	public void setManagerID(int managerID) {
+		this.managerID = managerID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 }
