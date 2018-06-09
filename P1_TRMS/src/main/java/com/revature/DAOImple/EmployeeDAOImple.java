3d7e69a0f2399eb75c158c684d75363a881719c0
unknown <dustinbayne51@gmail.com>
2018-06-09 19:09:14 -0400

    updated sql file

--- P1_TRMS/src/main/java/com/revature/DAOImple/EmployeeDAOImple.java
+++ P1_TRMS/src/main/java/com/revature/DAOImple/EmployeeDAOImple.java
@@ -1,5 +1,6 @@
 package com.revature.DAOImple;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,6 +27,8 @@ public class EmployeeDAOImple implements EmployeeDAO {
 			e = new Employee(rs.getInt(1),rs.getString(2),rs.getInt(3),rs.getInt(4),rs.getString(5),rs.getString(6));
 			employeeList.add(e);
 		}
+		rs.close();
+		conn.close();
 		return employeeList;
 	}
 	public void createEmployee(String userName,String password, String name, int positionType, int managerID) throws SQLException {
@@ -41,9 +44,55 @@ public class EmployeeDAOImple implements EmployeeDAO {
 		ps.setInt(4, positionType);
 		ps.setInt(5, managerID);
 		ps.executeUpdate();
+		ps.close();
+		conn.close();
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
+	}
+	//working on stored procedure for this.
+	public Employee getEmployeeByUsername(String username) throws SQLException {
+		Connection conn = cf.getConnection();
+		String sql = "SELECT * FROM EMPLOYEE WHERE EMPLOYEE.USERNAME=?";
+		
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, username);
+		ResultSet rs = ps.executeQuery();
+		Employee e = new Employee(rs.getInt(1),rs.getString(2),rs.getInt(3),rs.getInt(4),rs.getString(5),rs.getString(6));
+		rs.close();
+		ps.close();
+		conn.close();
+		return e;
+	}
+	public void updateEmployee(String oldUserName, String newUserName, String password, String name, int positionType,
+			int managerID) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection conn = cf.getConnection();
+		String sql = "{call UPDATE_EMPLOYEE(?,?,?,?,?,?)";
+		
+		CallableStatement call = conn.prepareCall(sql);
+		call.setString(1, oldUserName);
+		call.setString(2, newUserName);
+		call.setString(3, password);
+		call.setString(4, name);
+		call.setInt(5, positionType);
+		call.setInt(6, managerID);
+		call.execute();
+		conn.close();
+	}
+	public void deleteEmployee(String username) throws SQLException {
+		Connection conn = cf.getConnection();
+		String sql = "{call DELETE_EMPLOYEE(?)";
+		
+		CallableStatement call = conn.prepareCall(sql);
+		call.setString(1, username);
+		call.execute();
+		conn.close();
+		
+	}
+	public void getEmployeeUsernameAndPassword(String username) throws SQLException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
